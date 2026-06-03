f1a4d52c025a94e4a78606018dae4917c9af23b9
Jarvish1xyz <milans0415@gmail.com>
2026-06-03 13:09:17 +0530

    startting rust

---
+++ abc.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    int a,b, ans;
+
+    printf("Enter a : ");
+    scanf("%d", &a);
+
+    printf("Enter a : ");
+    scanf("%d", &b);
+
+    ans = a+b;
+
+    printf("The sum of a and b is : %d", ans);
+
+    return 0;
+}
